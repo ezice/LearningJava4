f0f7f48929c582c9260b5feeddf8122030bb7d3b
Rick Huff <rhuff@austin.utexas.edu>
2015-10-09 09:53:26 -0500

    Add chapter 2 stuff

---
+++ ch02/src/main/java/lj/ch02/HelloWorld.java
@@ -0,0 +1,15 @@
+package lj.ch02;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+        JFrame frame = new JFrame( "Hello, Java!" );
+        JLabel label = new JLabel("Hello, Java!", JLabel.CENTER );
+        frame.add(label);
+        frame.setSize( 300, 300 );
+        frame.setVisible( true );
+	}
+}
